139015fec930b61003541b97024a36828c1685a4
Nathan Macnamara <nathan.macnamara@datasift.com>
2015-07-09 11:22:57 +0100

    Adding basic versioning example set

---
+++ Examples/petstore.swagger.io.version/swagger-v2.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @SWG\Swagger(
+ *     schemes={"http"},
+ *     host="petstore.swagger.io",
+ *     basePath="/",
+ *     @SWG\Info(
+ *         @SWG\Version(ref="1.0", path="v1"),
+ *         @SWG\Version(ref="1.1", path="v1.1"),
+ *         @SWG\Version(ref="1.3", path="v1.3"),
+ *         @SWG\Version(ref="1.4", path="v1.4"),
+ *         title="Swagger Petstore",
+ *         description="This is a sample server Petstore server.  You can find out more about Swagger at <a href=""http://swagger.io"">http://swagger.io</a> or on irc.freenode.net, #swagger.  For this sample, you can use the api key ""special-key"" to test the authorization filters",
+ *         termsOfService="http://helloreverb.com/terms/",
+ *         @SWG\Contact(
+ *             email="apiteam@wordnik.com"
+ *         ),
+ *         @SWG\License(
+ *             name="Apache 2.0",
+ *             url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *         )
+ *     ),
+ *     @SWG\ExternalDocumentation(
+ *         description="Find out more about Swagger",
+ *         url="http://swagger.io"
+ *     )
+ * )
+ */
